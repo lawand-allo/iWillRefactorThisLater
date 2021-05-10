3345585231b0e4cd4862fa0f49ecdd3ae0eb782c
Allo <lawand.allo@sap.com>
2021-05-10 17:26:45 +0200

    bloaters:long method

--- src/main/Customer.java
+++ src/main/Customer.java
@@ -6,38 +6,59 @@ import java.util.*;
 public class Customer {
     private final String name;
     private final Vector rentals = new Vector();
-    public Customer (String newname){
+    int frequentRenterPoints = 0;
+    double totalAmount = 0;
+    String statement;
+
+    public Customer(String newname) {
         name = newname;
-    };
+    }
+
     public void addRental(Rental arg) {
         rentals.addElement(arg);
-    };
-    public String getName(){
-        return name;
-    };
-    public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
-        Enumeration enum_rentals = rentals.elements();
-        String result = "main.Rental Record for " + this.getName() + "\n";
-        result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public String statement() {
+        Enumeration enum_rentals = rentals.elements();
+        StringBuilder statementBuilder = new StringBuilder();
+        statementBuilder.append(getHeader());
+        statementBuilder.append(getTotalAmount(enum_rentals));
+        statementBuilder.append(getFooter());
+        return statementBuilder.toString();
+    }
+
+    private String getFooter() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Amount owed is ").append(totalAmount).append("\n");
+        stringBuilder.append("You earned ").append(frequentRenterPoints).append(" frequent renter points");
+        return stringBuilder.toString();
+    }
+
+    private String getHeader() {
+        StringBuilder statementBuilder = new StringBuilder("main.Rental Record for " + this.getName() + "\n");
+        statementBuilder.append("\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n");
+        return statementBuilder.toString();
+    }
+
+    private String getTotalAmount(Enumeration enum_rentals ) {
+        StringBuilder stringBuilder = new StringBuilder();
         while (enum_rentals.hasMoreElements()) {
             double thisAmount = 0;
             Rental each = (Rental) enum_rentals.nextElement();
             thisAmount = each.getCharge();
-            frequentRenterPoints ++;
+            frequentRenterPoints++;
             // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
-                frequentRenterPoints ++;
+            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+                frequentRenterPoints++;
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
+            stringBuilder.append("\t").append(each.getMovie().getTitle()).append("\t").append("\t").append(each.getDaysRented()).append("\t").append(String.valueOf(thisAmount)).append("\n");
             totalAmount += thisAmount;
         }
-        //add footer lines
-        result += "Amount owed is " + totalAmount + "\n";
-        result += "You earned " + frequentRenterPoints + " frequent renter points";
-        return result;
+        return stringBuilder.toString();
     }
 
 }
--- src/test/CustomerTest.java
+++ src/test/CustomerTest.java
@@ -4,9 +4,9 @@ import main.Rental;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.OutputStream;
-
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class CustomerTest {
     private Customer customer;
@@ -16,17 +16,58 @@ public class CustomerTest {
     }
 
     @Test
-    public void testRent() {
+    public void rentOneMovie() {
         String movieName = "The Godfather I";
         int daysRented = 2;
-        Rental rental = new Rental(new Movie(movieName,2),daysRented);
+        double amountForMovie = 7.0;
+        Rental rental = mock(Rental.class);
+        Movie movie = mock(Movie.class);
+        when(movie.getTitle()).thenReturn(movieName);
+        when(movie.getPriceCode()).thenReturn(Movie.REGULAR);
+        when(rental.getMovie()).thenReturn(movie);
+        when(rental.getCharge()).thenReturn(amountForMovie);
+        when(rental.getDaysRented()).thenReturn(daysRented);
         customer.addRental(rental);
-        String statement = "main.Rental Record for Mustermann\n" +
+        String expStatement = "main.Rental Record for Mustermann\n" +
                 "\tTitle\t\tDays\tAmount\n" +
-                "\t"+movieName+"\t\t"+daysRented+"\t1.5\n" +
-                "Amount owed is 1.5\n" +
+                "\t"+movieName+"\t\t"+daysRented+"\t"+amountForMovie+"\n" +
+                "Amount owed is "+amountForMovie+"\n" +
                 "You earned 1 frequent renter points";
-        System.out.println(statement);
-        assertEquals(statement,customer.statement());
+        System.out.println(expStatement);
+        assertEquals(expStatement,customer.statement());
+    }
+    @Test
+    public void rentTwoMovies() {
+        String movieName1 = "The Godfather I";
+        String movieName2 = "The Godfather II";
+        int daysRented = 2;
+
+        Rental rental1 = mock(Rental.class);
+        Rental rental2 = mock(Rental.class);
+        Movie movie1 = mock(Movie.class);
+        Movie movie2 = mock(Movie.class);
+        double amountForMovie1 = 2.0;
+        double amountForMovie2 = 2.5;
+        when(movie1.getTitle()).thenReturn(movieName1);
+        when(movie1.getPriceCode()).thenReturn(Movie.REGULAR);
+        when(movie2.getTitle()).thenReturn(movieName2);
+        when(movie2.getPriceCode()).thenReturn(Movie.NEW_RELEASE);
+        when(rental1.getMovie()).thenReturn(movie1);
+        when(rental2.getMovie()).thenReturn(movie2);
+        when(rental1.getCharge()).thenReturn(amountForMovie1);
+        when(rental2.getCharge()).thenReturn(amountForMovie2);
+        when(rental1.getDaysRented()).thenReturn(2);
+        when(rental2.getDaysRented()).thenReturn(2);
+        int frequentRenterPoints = 3;
+        double amountOwed = amountForMovie1 + amountForMovie2;
+        customer.addRental(rental1);
+        customer.addRental(rental2);
+        String expStatement = "main.Rental Record for Mustermann\n" +
+                "\tTitle\t\tDays\tAmount\n" +
+                "\t"+movieName1+"\t\t"+daysRented+"\t"+amountForMovie1+"\n" +
+                "\t"+movieName2+"\t\t"+daysRented+"\t"+amountForMovie2+"\n"+
+                "Amount owed is "+amountOwed+"\n" +
+                "You earned "+frequentRenterPoints+" frequent renter points";
+        assertEquals(expStatement,customer.statement());
     }
 }
